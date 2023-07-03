4cbcdc6b76e7365ebef3e16ceeb10b0b993a334a
Maryam Mamdouh <maryam.mamdouh@flairstech.com>
2023-07-03 15:00:14 +0300

    first commit

---
+++ sidebar/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
+import { routes } from "./routes";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          {routes}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
